53948e253636cedfc57644a94dea1549a7c610c2
ttaerrin <redgreenyell@naver.com>
2025-10-26 10:21:17 -0700

    my favorite restaurant

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          {'HELLENIKA'}<span role="img" aria-label="thumbs up">👍</span> is{count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
